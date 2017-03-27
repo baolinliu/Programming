8497253de32bde513ee3c939750a9209600f2d0d
baolinliu <baolin.liu@gmail.com>
2017-03-26 23:05:22 -0700

    Delete monthly.py

--- monthly.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-import random 
-
-def forecast():
-	return 'For the month of %s' % random.choice(['January','February','March','April','May','June','July','August','September', 'October','November','December'])
-
-	if __name__ == '__main__':
-		forecast()
